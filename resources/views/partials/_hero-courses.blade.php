be739636bbf9a57e21abcd3b85c6b16928929a48
Liliana Gorga <gorgaliliana87@gmail.com>
2023-11-11 11:02:06 +0100

    fix images directory and pagination style

--- resources/views/partials/_hero-courses.blade.php
+++ resources/views/partials/_hero-courses.blade.php
@@ -1,12 +1,12 @@
 <section class="relative h-72 flex flex-row items-center justify-center text-center space-y-4 mb-4">
     <div class="absolute left-0 px-24 max-[768px]:hidden">
-        <img src="{{ asset('storage/icon-1.png') }}" alt="_blank"/>
+        <img src="{{ asset('images/icon-1.png') }}" alt="_blank"/>
     </div>
     <div class="z-10 font font-bold">
         <h1 class="text-show py-14 mt-6 max-[768px]:px-3">Welcome into our teach world</h1>
         <p class="text-2xl text-dark my-4">Find your course</p>
     </div>
     <div class="absolute right-0 px-24 max-[768px]:hidden">
-        <img src="{{ asset('storage/icon-2.png') }}" alt="_blank"/>
+        <img src="{{ asset('images/icon-2.png') }}" alt="_blank"/>
     </div>
 </section>
